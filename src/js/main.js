633f28d29f6e190432541c88835551f9ffdb7a08
Jean-Baptiste RUDANT <jb@noknot.fr>
2017-05-31 11:20:52 +0200

    Format comments

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,9 @@ import ReactDOM from 'react-dom';
 import Root from './components/root'
 // We can import css from javascript with webpack `style-loader`
 import '../css/style.css'
-// Our app will be embeded in `index.html`; webpack file loader allows this kind of import to mark this 'dependence':
-// `index.html` will be automatically copied in the `dist` directory during the build process.
+// Our app will be embeded in `index.html`; webpack file loader allows this kind
+// of import to mark this 'dependence': `index.html` will be automatically
+// copied in the `dist` directory during the build process.
 import 'file-loader?name=[name].[ext]!../index.html'
 
 ReactDOM.render(
