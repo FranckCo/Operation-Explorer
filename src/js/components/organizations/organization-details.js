83d156cf8d37d022173dc81dd7ccb6fc22854e1c
Franck Cotton <franck.cotton@insee.fr>
2017-06-07 16:55:24 +0200

    Added web site to organization details

--- src/js/components/organizations/organization-details.js
+++ src/js/components/organizations/organization-details.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router';
 import { sparqlConnect } from 'sparql-connect';
 
 /**
   * Builds the query that retrieves the details on a given organization.
   */
 const queryBuilder = operation => `
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-  SELECT ?label
+  SELECT ?label ?seeAlso
   FROM <http://rdf.insee.fr/graphes/organisations>
   WHERE {
     <${operation}> skos:prefLabel ?label .
     FILTER (lang(?label) = 'fr')
+    OPTIONAL { <${operation}> rdfs:seeAlso ?seeAlso . }
   }
 `;
 
@@ -20,10 +23,13 @@ const connector = sparqlConnect(queryBuilder, {
   singleResult: true
 });
 
-function OrganizationDetails({ label }) {
+function OrganizationDetails({ label, seeAlso }) {
   return (
     <div>
       <h1>Organisation {label}</h1>
+      <p>Voir :&nsbp;
+        <a href={seeAlso}>site web</a>
+      </p>
     </div>
   );
 }
