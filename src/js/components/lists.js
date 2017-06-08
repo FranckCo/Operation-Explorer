3fca14523e22a4fe1900ac92f18ded1feddc8a73
Franck Cotton <franck.cotton@insee.fr>
2017-06-08 21:47:04 +0200

    Added series types

---
+++ src/js/components/lists.js
@@ -0,0 +1,9 @@
+
+export const operationTypes = {
+  "S": {"en": "Survey", "fr": "Enquête"},
+  "A": {"en": "Administrative data", "fr": "Données administratives"},
+  "C": {"en": "Synthesis", "fr": "Synthèse"},
+  "I": {"en": "Indicators", "fr": "Indicateurs"},
+  "P": {"en": "Panel", "fr": "Panel"},
+  "M": {"en": "Modelization", "fr": "Modélisation"}
+}
--- src/js/components/operations/series-details.js
+++ src/js/components/operations/series-details.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactTooltip from 'react-tooltip';
 import { sparqlConnect } from 'sparql-connect';
 import OperationsBySeries from './operations-by-series';
+import { operationTypes } from '../lists';
 
 /**
   * Builds the query that retrieves the details on a given operation series.
@@ -10,12 +11,13 @@ const queryBuilder = series => `
   PREFIX insee: <http://rdf.insee.fr/def/base#>
   PREFIX dcterms: <http://purl.org/dc/terms/>
   PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-  SELECT ?label ?abstract ?casd
+  SELECT ?label ?abstract ?type ?casd
   FROM <http://rdf.insee.fr/graphes/operations>
   WHERE {
     <${series}> skos:prefLabel ?label ; dcterms:abstract ?abstract ; insee:casdAvailable ?casd.
     FILTER (lang(?label) = 'fr')
     FILTER (lang(?abstract) = 'fr')
+    OPTIONAL {<${series}> dcterms:type ?type}
   }
 `;
 
@@ -25,8 +27,7 @@ const connector = sparqlConnect(queryBuilder, {
   singleResult: true
 });
 
-function SeriesDetails({ series, label, abstract, casd }) {
-  console.log(typeof casd);
+function SeriesDetails({ series, label, abstract, type, casd }) {
   return (
     <div>
       <h1>Série {label}
@@ -36,6 +37,8 @@ function SeriesDetails({ series, label, abstract, casd }) {
       </h1>
       <ReactTooltip />
       <h2>{abstract}</h2>
+      <p className="badge">{operationTypes[type.slice(-1)].fr}</p>
+      <br/>
       <OperationsBySeries series={series} />
     </div>
   );
