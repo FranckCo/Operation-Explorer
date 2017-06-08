ccb6e7a433127a02485f44a04d7b88ddde3ca827
Franck Cotton <franck.cotton@insee.fr>
2017-06-08 22:15:49 +0200

    First shot at organization mother

--- src/js/components/organizations/organization-details.js
+++ src/js/components/organizations/organization-details.js
@@ -8,12 +8,15 @@ import { sparqlConnect } from 'sparql-connect';
 const queryBuilder = operation => `
   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-  SELECT ?label ?seeAlso
+  PREFIX dcterms: <http://purl.org/dc/terms/>
+  PREFIX org: <http://www.w3.org/ns/org#>
+  SELECT ?label ?seeAlso ?mother
   FROM <http://rdf.insee.fr/graphes/organisations>
   WHERE {
     <${operation}> skos:prefLabel ?label .
     FILTER (lang(?label) = 'fr')
     OPTIONAL { <${operation}> rdfs:seeAlso ?seeAlso . }
+    OPTIONAL { <${operation}> org:unitOf ?mother . }
   }
 `;
 
@@ -23,13 +26,15 @@ const connector = sparqlConnect(queryBuilder, {
   singleResult: true
 });
 
-function OrganizationDetails({ label, seeAlso }) {
+function OrganizationDetails({ label, seeAlso, mother }) {
+  console.log(mother);
   return (
     <div>
       <h1>Organisation {label}</h1>
       <p>Voir :&nsbp;
         <a href={seeAlso}>site web</a>
       </p>
+      {mother && <p>{mother}</p>}
     </div>
   );
 }
