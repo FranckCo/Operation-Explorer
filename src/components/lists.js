6b827391273747e78d5b9a972d239ec7602a8b73
Franck Cotton <franck.cotton@insee.fr>
2017-06-20 10:12:11 +0300

    Added series periodicity

--- src/components/lists.js
+++ src/components/lists.js
@@ -7,3 +7,14 @@ export const operationTypes = {
   "P": {"en": "Panel", "fr": "Panel"},
   "M": {"en": "Modelization", "fr": "Modélisation"}
 }
+
+export const periodicities = {
+  "A": {"en": "Annual", "fr": "Annuelle"},
+  "P": {"en": "Aperiodic", "fr": "Apériodique"},
+  "C": {"en": "Continuous", "fr": "En continu"},
+  "M": {"en": "Monthly", "fr": "Annuelle"},
+  "Q": {"en": "Quarterly", "fr": "Trimestrielle"},
+  "S": {"en": "Biannual", "fr": "Semestrielle"},
+  "PA": {"en": "Multiannual", "fr": "Multiannuelle"},
+  "PO": {"en": "Punctual", "fr": "Ponctuelle"}
+}
--- src/components/operations/series-details.js
+++ src/components/operations/series-details.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactTooltip from 'react-tooltip';
 import { sparqlConnect } from 'sparql-connect';
 import OperationsBySeries from './operations-by-series';
-import { operationTypes } from '../lists';
+import { operationTypes, periodicities } from '../lists';
 
 /**
   * Builds the query that retrieves the details on a given operation series.
@@ -11,13 +11,14 @@ const queryBuilder = series => `
   PREFIX insee: <http://rdf.insee.fr/def/base#>
   PREFIX dcterms: <http://purl.org/dc/terms/>
   PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-  SELECT ?label ?abstract ?type ?casd
+  SELECT ?label ?abstract ?type ?casd ?periodicity
   FROM <http://rdf.insee.fr/graphes/operations>
   WHERE {
     <${series}> skos:prefLabel ?label ; dcterms:abstract ?abstract ; insee:casdAvailable ?casd.
     FILTER (lang(?label) = 'fr')
     FILTER (lang(?abstract) = 'fr')
     OPTIONAL {<${series}> dcterms:type ?type}
+    OPTIONAL {<${series}> dcterms:accrualPeriodicity ?periodicity}
   }
 `;
 
@@ -27,7 +28,7 @@ const connector = sparqlConnect(queryBuilder, {
   singleResult: true
 });
 
-function SeriesDetails({ series, label, abstract, type, casd }) {
+function SeriesDetails({ series, label, abstract, type, casd, periodicity }) {
   return (
     <div>
       <h1>Série {label}
@@ -37,7 +38,8 @@ function SeriesDetails({ series, label, abstract, type, casd }) {
       </h1>
       <ReactTooltip />
       <h2>{abstract}</h2>
-      <p className="badge">{operationTypes[type.slice(-1)].fr}</p>
+      <p className="label label-pill label-primary">{operationTypes[type.slice(-1)].fr}</p>
+      <p className="label label-pill label-info">{periodicities[periodicity.split("/").pop()].fr}</p>
       <br/>
       <OperationsBySeries series={series} />
     </div>
