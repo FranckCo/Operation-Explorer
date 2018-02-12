a60fb4f0d09fe3ea97c4f601457580c88fe7ab70
Nicolas Laval <nicolas.laval@insee.fr>
2018-02-12 16:40:03 +0100

    Switch config to stardog, repo casd

--- src/config.js
+++ src/config.js
@@ -7,12 +7,12 @@ export default {
   // baseHost: 'http://id.insee.fr'
 
   // Stardog - Repo : operations-casd
-  // withAuth: true,
-  // queryURL: 'https://unece-stardog.ichec.ie/annex/operations-casd/sparql/query',
-  // baseHost: 'http://id.insee.fr'
+  withAuth: true,
+  queryURL: 'https://unece-stardog.ichec.ie/annex/operations-casd/sparql/query',
+  baseHost: 'http://id.insee.fr'
 
   // Insee QF - Repo : data
-  withAuth: false,
-  queryURL: 'http://qfrmessnczlht01/sparql',
-  baseHost: 'http://qfrmessnczlht01'
+  // withAuth: false,
+  // queryURL: 'http://qfrmessnczlht01/sparql',
+  // baseHost: 'http://qfrmessnczlht01'
 }
