9c9d9f1281872bf55016a3e5250214491a2e3cea
Nicolas Laval <nicolas.laval@insee.fr>
2018-03-01 16:23:58 +0100

    Add SCFE config

--- src/config.js
+++ src/config.js
@@ -1,15 +1,20 @@
 // Parameters for the application
 
 export default {
+  // SCFE - Repo : INSEE-OPS
+  withAuth: false,
+  queryURL: 'http://apps.scfe.eu/fuseki/INSEE-OPS/query',
+  baseHost: 'http://id.insee.fr'
+
   // Stardog - Repo : operations
   // withAuth: true,
   // queryURL: 'https://unece-stardog.ichec.ie/annex/operations/sparql/query',
   // baseHost: 'http://id.insee.fr'
 
   // Stardog - Repo : operations-casd
-  withAuth: true,
-  queryURL: 'https://unece-stardog.ichec.ie/annex/operations-casd/sparql/query',
-  baseHost: 'http://id.insee.fr'
+  // withAuth: true,
+  // queryURL: 'https://unece-stardog.ichec.ie/annex/operations-casd/sparql/query',
+  // baseHost: 'http://id.insee.fr'
 
   // Insee QF - Repo : data
   // withAuth: false,
