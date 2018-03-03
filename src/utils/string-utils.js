36e1d927241479fafdd746e2532e5cd5b85fd861
Nicolas Laval <nicolas.laval@insee.fr>
2018-03-03 13:52:50 +0100

    Fiw minor issue with iterator key

--- src/utils/string-utils.js
+++ src/utils/string-utils.js
@@ -1,5 +1,4 @@
 import React from 'react';
 
-export const tidyString = string => (
-	string && <span>{string.split(/\n/).map(s => <p>{s}</p>)}</span>
-);
+export const tidyString = string =>
+	string && <span>{string.split(/\n/).map((s, i) => <p key={i}>{s}</p>)}</span>;
